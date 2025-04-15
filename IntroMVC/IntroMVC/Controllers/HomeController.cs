4f937898e0d954ddf1c585f1be54ae4f0d08a580
Nahid Fahim <fahimbinimam@gmail.com>
2025-04-15 23:30:13 +0600

    updated intro mvc

--- IntroMVC/IntroMVC/Controllers/HomeController.cs
+++ IntroMVC/IntroMVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IntroMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -24,7 +25,34 @@ namespace IntroMVC.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            TempData["msg"] = "Redirected from Contact";
+            return RedirectToAction("Students");
             return View();
         }
+
+        public ActionResult Students()
+        {
+            ViewBag.s1Name = "Nahid";
+
+            ViewData["s2Name"] = "Sakib";
+
+            Student s1 = new Student(){
+                Id = 1,
+                Name = "Nahid"
+            };
+
+            Student s2 = new Student()
+            {
+                Id = 2,
+                Name = "Fahim"
+            };
+
+        Student[] students = new Student[] {s1 , s2};
+
+
+        
+
+            return View(students);
+        }   
     }
 }
