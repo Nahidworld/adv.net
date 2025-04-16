d06a2af5d72ed354de60cbdb8a4919ccb417fa3e
Nahid Fahim <fahimbinimam@gmail.com>
2025-04-16 11:59:13 +0600

    form

---
+++ FormSubmission/FormSubmission/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using FormSubmission.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormSubmission.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new Student());
+        }
+
+        [HttpPost]
+        public ActionResult Index(Student s)
+        {
+            if (ModelState.IsValid)
+            {
+                //return RedirectToAction("Index", "Home");
+                ViewBag.V = "Valid";
+            }
+            return View(s);
+        }
+    }
+}
